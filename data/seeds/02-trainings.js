7fea33305ba042065b2e6b6b10125a12788d6eca
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-11-19 17:16:00 -0800

    populated database with users

--- data/seeds/02-trainings.js
+++ data/seeds/02-trainings.js
@@ -10,6 +10,18 @@ exports.seed = function (knex) {
           date: "11-24-2020",
           time: "2:00PM",
         },
+        {
+          name: "React",
+          date: "11-27-2020",
+          time: "3:30PM",
+          notes: "Intro to React",
+        },
+        {
+          name: "CS",
+          date: "12-16-2020",
+          time: "2:45PM",
+          notes: "Intro to Python, C++, and algorithms",
+        },
       ]);
     });
 };
